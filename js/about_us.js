43aa825ed522801f7ddb80b8b0bc4d04b681c0f9
jsj08020 <jsj08020@naver.com>
2025-05-23 00:36:42 +0900

    feat: about_us 페이지 작업

---
+++ js/about_us.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll(".buttons button");
+const people = document.querySelectorAll(".people > div");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const selectedDept = button.id;
+
+    people.forEach(card => {
+      if (card.className.startsWith(selectedDept)) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
