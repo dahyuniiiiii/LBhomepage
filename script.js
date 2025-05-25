fc9d3c85c3d991876d4d6521e5ec29ffbd1da2ea
dahyuniiiiii <imcute0703123@naver.com>
2025-05-26 00:29:40 +0900

    fix:26일 0시 기준 커밋내용 병합 및 메인 페이지 디자인, 기능 수정

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const buttons = document.querySelectorAll(".buttons button");
 const people = document.querySelectorAll(".people > div");
 
+window.addEventListener("DOMContentLoaded", () => {
+  people.forEach(card => {
+    const className = card.className;
+    if (className.includes("PM-")) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  });
+  buttons.forEach(btn => btn.classList.remove("active"));
+  document.getElementById("PM").classList.add("active");
+});
+
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     const selectedDept = button.id;
 
     people.forEach(card => {
-      if (card.className.startsWith(selectedDept)) {
+      const className = card.className;
+      if (className.includes(`${selectedDept}-`)) {
         card.style.display = "flex";
       } else {
         card.style.display = "none";
       }
     });
+
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
   });
 });
-
