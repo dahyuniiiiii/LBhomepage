e4e3ae3c4728614d6646c6c0526d6d71ddd2338a
Shalo0325 <cmj4247327@gmail.com>
2025-05-27 02:24:25 +0900

    feat: 리뷰 페이지 모달 창 구현

---
+++ review.js
@@ -0,0 +1,9 @@
+// 폼 열기
+function openForm() {
+  document.getElementById("reviewForm").style.display = "flex";
+}
+
+// 폼 닫기
+function closeForm() {
+  document.getElementById("reviewForm").style.display = "none";
+}
