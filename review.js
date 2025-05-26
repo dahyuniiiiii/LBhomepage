d79843e06ea938141a283b3fb22b9c7b66102035
dahyuniiiiii <imcute0703123@naver.com>
2025-05-27 03:59:17 +0900

    fix: 리뷰페이지 모달창 구현 확인

---
+++ review.js
@@ -0,0 +1,8 @@
+function openForm() {
+  document.getElementById("reviewForm").style.display = "flex";
+}
+
+// 폼 닫기
+function closeForm() {
+  document.getElementById("reviewForm").style.display = "none";
+}
