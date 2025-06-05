84773ee3ea6fc818b654ff1d28c20ae146c61e22
dahyuniiiiii <imcute0703123@naver.com>
2025-06-05 23:55:05 +0900

    chore: 프로젝트 배포 코드 업로드

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
+
 const buttons = document.querySelectorAll(".buttons button");
 const people = document.querySelectorAll(".people > div");
 
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+
+  window.showModal1 = function () {
+    const modal = document.getElementById("projectModal");
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  };
+
+  window.showModal2 = function () {
+    const modal = document.getElementById("projectModal2");
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  };
+
+  window.closeModal1 = function () {
+    const modal = document.getElementById("projectModal");
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  };
+
+  window.closeModal2 = function () {
+    const modal = document.getElementById("projectModal2");
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  };
+
   const user = localStorage.getItem('User');
   if (user) {
     document.getElementById('popup').style.display = 'block';
     document.getElementById('user').innerText = user;
-    localStorage.removeItem('User'); 
+    const loginBtn = document.getElementById('loginBtn');
+    loginBtn.innerText = '로그아웃';
+    loginBtn.onclick = () => {
+      localStorage.removeItem('User');
+      window.location.reload();
+    };
+  setTimeout(() => {
+      closePopup();
+    }, 3000);
   }
-});
-
-function closePopup() {
-  document.getElementById('popup').style.display = 'none';
-}
-
-setTimeout(() => {
-  closePopup();
-}, 3000);
-
-window.addEventListener("DOMContentLoaded", () => {
   people.forEach(card => {
     const className = card.className;
     if (className.includes("PM-")) {
@@ -30,6 +53,12 @@ window.addEventListener("DOMContentLoaded", () => {
   buttons.forEach(btn => btn.classList.remove("active"));
   document.getElementById("PM").classList.add("active");
 });
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
